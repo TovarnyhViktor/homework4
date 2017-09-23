ff82fabe83f794caf3d0ee90474e6e8161c9cf4b
TovarnyhViktor <milasalvska2@gmail.com>
2017-09-23 12:14:56 +0300

    Задание 2

---
+++ src/ex2.java
@@ -0,0 +1,17 @@
+public class ex2 {
+        static String drawRectangle(int width, int height, char c) {
+            String result = "";
+            for(int i = 0; i < height; i++) {
+                String line = "";
+                for(int j = 0; j < width; j++) {
+                    line += c;
+                }
+                result += line + "\n";
+            }
+            return result;
+        }
+        public static void main(String[] args) {
+            System.out.println(drawRectangle(3, 2, '+'));
+        }
+}
+
