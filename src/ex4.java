1f729736d9abc65d3e8f6e52c34779d681e0ece9
TovarnyhViktor <milasalvska2@gmail.com>
2017-09-25 22:20:40 +0300

    Задание 4

--- src/ex4.java
+++ src/ex4.java
@@ -1,32 +1,36 @@
-import java.util.Scanner;
-public class ex4 {
-    public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        System.out.print("Введите a: ");
-        int a = in.nextInt();
-        System.out.print("Введите b: ");
-        int b = in.nextInt();
-        int c = maxfunctionint(a,b);
-        int d = maxfunctionfloat(a,b);
-        System.out.println(c);
-        System.out.println(d);
-    }
-    public static int maxfunctionint(int a, int b) {
-        int max1;
-        if (a < b)
-            max1 = b;
-        else
-            max1 = a;
+    import java.util.Scanner;
+    public class ex4 {
+        public static void main(String[] args){
+            Scanner in = new Scanner(System.in);
+            System.out.print("Введите a: ");
+            int a = in.nextInt();
+            System.out.print("Введите b: ");
+            int b = in.nextInt();
+            System.out.print("Введите a float: ");
+            float c = in.nextFloat();
+            System.out.print("Введите b float: ");
+            float d = in.nextFloat();
+            int e = max(a,b);
+            float f = max1(c,d);
+            System.out.println(e);
+            System.out.println(f);
+        }
+        public static int max(int a, int b) {
+            int max;
+            if (a < b)
+                max = b;
+            else
+                max = a;
 
-        return max1;
-    }
-    public static int maxfunctionfloat(float a, float b) {
-        int max;
-        if (a < b)
-            max = b;
-        else
-            max = a;
+            return max;
+        }
+        public static float max1(float c, float d) {
+            float max1;
+            if (c < d)
+                max1 = d;
+            else
+                max1 = c;
 
-        return max;
+            return max1;
+        }
     }
-}
